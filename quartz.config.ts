c21ea60c6ef3d94fdfd4bf9e0c0fcb5de19d8c4c
LSDBC <lorenzobianchichignoli@gmail.com>
2025-11-05 15:30:37 +0100

    Update domain

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://notebook.bianchichignoli.it",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
